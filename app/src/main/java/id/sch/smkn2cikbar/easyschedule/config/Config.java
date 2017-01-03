f8472bda6d1fd05355803954bc148027ee8de731
Robby Akbar <robbyakbar0@gmail.com>
2017-01-04 01:52:37 +0700

    Update Config.java

--- app/src/main/java/id/sch/smkn2cikbar/easyschedule/config/Config.java
+++ app/src/main/java/id/sch/smkn2cikbar/easyschedule/config/Config.java
@@ -14,12 +14,7 @@ public class Config {
     public static final String PENGUMUMAN = Config.SERVER_API +"list_pengumuman.php";
     public static final String SIGN_UP = Config.SERVER_API +"insert_siswa.php";
     public static final String JADWAL_SISWA = Config.SERVER_API +"list_jadwal_siswa.php?kode_kelas=";
-    public static final String ABSENSI_LIST = Config.SERVER_API +"list_absensi_siswa.php?nis=";
     public static final String JADWAL_GURU = Config.SERVER_API +"list_jadwal_guru.php?nip=";
-    public static final String PILIH_SISWA = Config.SERVER_API +"list_siswa.php?nip=";
-    public static final String UJIAN_SISWA = Config.SERVER_API +"list_jadwal_ujian.php";
-    public static final String NILAI_SISWA = Config.SERVER_API +"list_nilai_siswa.php?nis=";
-    public static final String INSERT_ABSENSI = Config.SERVER_API +"insert_absensi.php";
     public static final String NIP = "nip";
     public static final String NIS = "nis";
     public static final String KEY_PASS = "password";
@@ -28,22 +23,6 @@ public class Config {
     public static final String USERNAME_SHARED_PREF_GURU = "nip";
     public static final String USERNAME_SHARED_PREF_SISWA = "nis";
     public static final String LOGGEDIN_SHARED_PREF = "loggedin";
-    public static final String TAG_NAMA_GURU="nama_guru";
     public static final String TAG_KELAS="kelas";
-    public static final String TAG_SUB_KELAS="sub_kelas";
-    public static final String TAG_MAPEL="mapel";
-    public static final String TAG_NIS="nis";
-    public static final String TAG_NAMA_SISWA="nama_siswa";
-    public static final String TAG_KODE_MAPEL="kode_mapel";
     public static final String TAG_HARI="hari";
-    public static final String TAG_JAM_MULAI="jam_mulai";
-    public static final String TAG_JAM_SELESAI="jam_selesai";
-    public static final String TAG_TGL="tgl_ujian";
-    public static final String TAG_KET="ket";
-    public static final String TAG_N_TUGAS="nilai_tugas";
-    public static final String TAG_N_UH="nilai_uh";
-    public static final String TAG_N_UTS="nilai_uts";
-    public static final String TAG_N_UAS="nilai_uas";
-    public static final String TAG_N_AKHIR="nilai_akhir";
-
 }
